469a60fc87e40b1bad73a0c78308e6955202d208
GauravGoswami12967 <gaurav12967@gmail.com>
2024-02-13 10:07:16 +0530

    changes

---
+++ React/taskreact/src/App.js
@@ -0,0 +1,25 @@
+import Class_compo from "./Component/Class_compo";
+import Func_compo from "./Component/Func_compo";
+
+function App() {
+  return (
+    <div className="App">
+      {
+       
+        //<h1>Hi i am app.js component</h1>  
+      
+       // componet type
+        
+      }
+      
+      
+        <Func_compo/>
+        <Class_compo/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ React/taskreact/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import Custome from './Custome';
+
+const root = ReactDOM.createRoot(document.getElementById('raj'));
+root.render(<>
+            {
+            //<h1>Hi i am index.js</h1>
+            <App/>
+            //<Custome/>
+            }
+            </>);
+
